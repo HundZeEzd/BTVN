fcee1e581c02b6e685382c88d0bf9f123f751a5f
HundZeEzd <84385734+HundZeEzd@users.noreply.github.com>
2021-05-29 16:23:08 +0700

    Add files via upload

--- Lesson 2+3/StringSoLanLapLai.java
+++ Lesson 2+3/StringSoLanLapLai.java
@@ -1,1 +1,23 @@
+import java.util.Scanner;
 
+public class StringSoLanLapLai {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Nhap xau ki tu: ");
+        String a = input.next();
+        int num = a.length();
+        char[] arr = new char[a.length()];
+        for (int i = 0; i < num; i++) {
+            arr[i] = a.charAt(i);
+        }
+        for (int i = 0; i < num; i++) {
+            int count = 0;
+            for (int j = 0; j < num; j++) {
+                if (arr[i] == arr[j]) {
+                    count++;
+                }
+            }
+            System.out.println("So lan lap lai cua " + arr[i] + ": " + count);
+        }
+    }
+}
