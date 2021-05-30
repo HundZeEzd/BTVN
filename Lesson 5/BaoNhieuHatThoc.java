6e30b3729c58e5147755160be1b47f23bdcb5c74
HundZeEzd <84385734+HundZeEzd@users.noreply.github.com>
2021-05-30 19:27:22 +0700

    Add files via upload

---
+++ Lesson 5/BaoNhieuHatThoc.java
@@ -0,0 +1,10 @@
+public class BaoNhieuHatThoc {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        for (int i = 1; i <= 15; i++)
+            a = a * b;
+        a = a - 1;
+        System.out.println(a);
+    }
+}
